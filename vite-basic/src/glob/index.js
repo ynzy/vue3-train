85deb0667326621e353af5cad2c9d6163083b45a
zhangyong <zhangyong@uino.com>
2022-04-14 16:01:25 +0800

    feat: glob

--- vite-basic/src/glob/index.js
+++ vite-basic/src/glob/index.js
@@ -1,2 +1,4 @@
-const globalModules = import.meta.globEager('./*-[0-9].js');
-console.log('global', globalModules);
+const globEagerModules = import.meta.globEager('./*-[0-9].js');
+// console.log('globEagerModules', globEagerModules);
+const globModules = import.meta.glob('./*');
+// console.log('globModules', globModules);

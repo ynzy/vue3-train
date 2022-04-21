5b572ec524f99f569be029e3454c4cdc89c27713
ynzy <758922096@qq.com>
2022-04-21 15:22:52 +0800

    feat: 断言inputEvent

--- vite-jsx/src/components/Directives/CustomInput.vue
+++ vite-jsx/src/components/Directives/CustomInput.vue
@@ -1,10 +1,20 @@
-<script>
+<script lang="ts">
 export default {
   props: ['modelValue'],
-  emits: ['update:modelValue']
+  emits: ['update:modelValue'],
+  setup(props, { emit }) {
+    const inputChange = (event: Event) => {
+      // 类型断言     断定event.target一定是html input标签   不然不能点value
+      const target = event.target as HTMLInputElement;
+      emit('update:modelValue', target.value);
+    };
+    return {
+      inputChange
+    };
+  }
 };
 </script>
 
 <template>
-  <input :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" />
+  <input :value="modelValue" @input="inputChange" />
 </template>
--- vite-jsx/src/components/Directives/VueDirectives.tsx
+++ vite-jsx/src/components/Directives/VueDirectives.tsx
@@ -1,26 +1,26 @@
 import { defineComponent, ref, watch, watchEffect } from 'vue';
 import CustomInput from './CustomInput.vue';
 
-const Layout = (label)=> <div class='layout'>
+const Layout = (label) => <div class='layout'>
   <div class='left'>左边</div>
   <div class='right'>右边</div>
 </div>
 
 export default defineComponent({
   setup() {
-      const modelValue = ref('')
-      const searchText = ref('')
-      const trimValue = ref(' ')
-      watch(modelValue,(v)=>{
-        console.log('modelValue',v);
-      })
-      watch(searchText,(v)=>{
-        console.log('searchText',v);
-      })
-      watch(trimValue,(v)=>{
-        console.log('trimValue',v,v.length);
-      })
-      
+    const modelValue = ref('')
+    const searchText = ref('')
+    const trimValue = ref(' ')
+    watch(modelValue, (v) => {
+      console.log('modelValue', v);
+    })
+    watch(searchText, (v) => {
+      console.log('searchText', v);
+    })
+    watch(trimValue, (v) => {
+      console.log('trimValue', v, v.length);
+    })
+
     return () => {
       return <>
         {/* 正常写法 */}
@@ -29,14 +29,14 @@ export default defineComponent({
         <br />
         {/* 指定值写法 */}
         {/* <CustomInput v-model:modelValue="searchText"/> // vue */}
-        <CustomInput v-model:modelValue={searchText.value}/>
+        <CustomInput v-model:modelValue={searchText.value} />
         <br />
         {/* 修饰符写法 */}
         {/* <input v-model:modelValue.trim="value" />  // vue */}
-        <input v-model={[trimValue.value,['trim']]} />
-        <CustomInput v-model={[searchText.value,'modelValue',['trim']]}/>
+        <input v-model={[trimValue.value, ['trim']]} />
+        <CustomInput v-model={[searchText.value, 'modelValue', ['trim']]} />
         {/*  */}
-        <Layout label='正常写法'/>
+        {/* <Layout label='正常写法'/> */}
       </>;
     };
   }
